6e5c9eb1e3869db259ce9c99b22fe078bdf35e0c
Gavin <gavinwan0929@gmail.com>
2023-04-18 11:20:58 -0400

    2updated

--- Starter_Code/static/js/config.js
+++ Starter_Code/static/js/config.js
@@ -1,19 +1,36 @@
-// // API key 6581708e1fmshd5c5cf3a0d95bbbp1bad2bjsn690eae7c6160
-// var apiKey = '6581708e1fmshd5c5cf3a0d95bbbp1bad2bjsn690eae7c6160';
-// var url = 'national-weather-service.p.rapidapi.com' + apiKey;
+ // API key 
+var apiKey = '6581708e1fmshd5c5cf3a0d95bbbp1bad2bjsn690eae7c6160';
+var url = 'everyearthquake.p.rapidapi.com' + apiKey;
 
 // d3.json(url).then(function(data) {
 //   // process the GeoJSON data
 // });
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6581708e1fmshd5c5cf3a0d95bbbp1bad2bjsn690eae7c6160',
-		'X-RapidAPI-Host': 'national-weather-service.p.rapidapi.com'
-	}
-};
 
-fetch('https://national-weather-service.p.rapidapi.com/zones/%7Btype%7D/%7BzoneId%7D/forecast', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// example 2:
+// const axios = require("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://everyearthquake.p.rapidapi.com/earthquakes',
+//   params: {
+//     start: '1',
+//     count: '100',
+//     type: 'earthquake',
+//     latitude: '33.962523',
+//     longitude: '-118.3706975',
+//     radius: '1000',
+//     units: 'miles',
+//     magnitude: '3',
+//     intensity: '1'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '6581708e1fmshd5c5cf3a0d95bbbp1bad2bjsn690eae7c6160',
+//     'X-RapidAPI-Host': 'everyearthquake.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+});
